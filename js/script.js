e86ee3690bfd59a751f6a4f4123b95b77219ac67
jonathan@produzdigital.com.br <jonathan@produzdigital.com.br>
2020-05-10 21:32:00 -0300

    API createHotel AppendChild

--- js/script.js
+++ js/script.js
@@ -6,15 +6,40 @@ AppHotel();
 async function fetchHotel(url) {
     const hotelResponse = await fetch(url);
     const hotelJSON = await hotelResponse.json();
+    const divBlog = document.querySelector('.blog');
 
     //console.log(hotelJSON);
     hotelJSON.forEach(hotel => {
-        createHotel(hotel);
+        const divHotel = createHotel(hotel);
+        //console.log(divHotel);
+
+        divBlog.appendChild(divHotel);
     });
 }
 
 function createHotel(hotel) {
-    console.log(hotel);
+    //console.log(hotel);
+    const div = document.createElement('div');
+    div.classList.add('blog-post');
+    div.innerHTML = `
+    <a href="#">
+        <div class="blog-img">
+        <img src="${hotel.photo}" alt="">
+        </div>
+        <div class="blog-contet-post">
+            <div class="blog-cat">
+                <p>${hotel.property_type}</p>
+            </div>
+            <div class="blog-title">
+                <h3>${hotel.name}</h3>
+            </div>
+            <div class="blog-text">
+                <p>R$ ${hotel.price},00</p>
+            </div>
+        </div>
+    </a>
+    `;
+    return div;
 }
 
 fetchHotel('https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72');
